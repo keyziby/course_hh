610f409ea13305b67951b5140fd941d6fc5d23b4
alinadolbilova <genin.10@mail.ru>
2024-02-11 18:21:59 +0400

    add_config

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+ROOT_DIR = os.path.dirname(__file__)
+OPERATIONS_PATH = os.path.join(ROOT_DIR, "hh_vac.json")
+API_SJ = "v3.r.138118518.5a9fdc85d44c251fc2b3509b6c61899574ddee21.5ae09cefe26b2cfc4a997e1dccbd5f8a85e3c800"
+HH_URL = "https://api.hh.ru/vacancies"
+SJ_URL = "https://api.superjob.ru/2.0/vacancies/"
